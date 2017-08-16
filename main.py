50fab0803fe601e91610ffc489fb4d23bc4ae8ae
Deni Shakhbulatov <dshakhbu@nyit.edu>
2017-08-16 14:02:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Python 2.7
 import flickrapi
 from bs4 import BeautifulSoup
 import requests
@@ -16,10 +17,11 @@ def main():
 		os.makedirs('images')
 	os.chdir('images')
 	download()
-	print('All set, all pictures were downloaded into a folder called pics')
+	print('All set, all pictures were downloaded into a folder called images')
 
 def download():
 	choice = (raw_input('Type "tag" or "album" for corresponding choice. \nDo you want to download images by tag or specific album: '))
+	#counter is created in order to label the images when they are downloaded
 	counter = 0
 
 	if(choice == 'album'):
@@ -30,15 +32,20 @@ def download():
 			os.makedirs(name+'/'+albumID)
 		os.chdir(name+'/'+albumID)
 
-		print('download is started')
+		print('Downloading...')
+		# walk_set function loops through the pictures of a specific album
 		for photo in flickr.walk_set(albumID):
+			# beautiful soup opens up the direct link to the picture using authors id(name) and photo id, specifying sizes/k will
+			# result in the highest quality picture available on flickr
 			url = 'https://www.flickr.com/photos/'+ name+ '/' + photo.get('id') + '/sizes/k/'
 			webpage = requests.get(url)
 			soup = BeautifulSoup(webpage.text, 'html.parser')
 			x = soup.findAll('img')
+			# we read the html using soup and look for img, after which we look for src link and extract it
 			for link in soup.find_all('img'):
 				new = (link.get('src'))
 				if(new.count(".jpg")) == 1:
+					#the link is downloaded using URLopener() and saved with 'photo + counter'
 					testfile = urllib.URLopener()
 					testfile.retrieve(new, 'photo' + str(counter) + '.jpg' )
 					counter = counter + 1
@@ -49,17 +56,23 @@ def download():
 		if not os.path.exists(tag):
 			os.makedirs(tag)
 		os.chdir(tag)
+		# checking the total number of available pictures with the specific tag
 		total = int(flickr.photos.search(tags=tag).find('photos').attrib['total'])			
 		print('There are ' + str(total) + ' pictures found \nDownloading...')
+		# walk_set function loops through the pictures with the tag for more info go to flickrapi python documentation
 		for photo in flickr.walk(tag_mode='all', tags=tag):
-			author =  photo.get('owner')
+			author =  photo.get('owner') # return the owner of the picture
+			# beautiful soup opens up the direct link to the picture using authors id and photos id, specifying sizes/k will
+			# result in the highest quality picture available on flickr
 			url = 'https://www.flickr.com/photos/'+ author+ '/' + photo.get('id') + '/sizes/k/'
 			webpage = requests.get(url)
 			soup = BeautifulSoup(webpage.text, 'html.parser')
 			x = soup.findAll('img')
+			# we read the html using soup and look for img, after which we look for src link and extract it
 			for link in soup.find_all('img'):
 				new = (link.get('src'))
 				if(new.count(".jpg")) == 1:
+					#the link is downloaded using URLopener() and saved with 'photo + counter'
 					testfile = urllib.URLopener()
 					testfile.retrieve(new, 'photo' + str(counter) + '.jpg' )
 					counter = counter + 1
